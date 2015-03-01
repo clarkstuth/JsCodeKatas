c38459c36ac1233968edfdfc5818ffe14aba8223
Clark Stuth <clarkstuth@gmail.com>
2015-03-01 21:43:52 +0000

    Got a few tests passing and performed a refactor.

--- string_calculator/src/calculator.js
+++ string_calculator/src/calculator.js
@@ -3,7 +3,16 @@ function Calculator() {
 }
 
 Calculator.prototype = {
+
 	add: function(str) {
-		return 2;
+		var parts = str.split(",");
+		var sum = 0;
+
+		for (var i = 0; i < parts.length; i++) {
+			var num = +parts[i];
+			sum += num;
+		}
+
+		return sum;
 	}
 }
--- string_calculator/test/calculator_test.js
+++ string_calculator/test/calculator_test.js
@@ -1,13 +1,29 @@
 var expect = chai.expect;
 
 describe("Calculator", function() {
+
+	var calc = null;
+
+	before(function() {
+		calc = new Calculator();
+	})
+
 	describe("Add", function() {
-		it("should return 2 when given 1+1", function() {
-			var calculator = new Calculator();
 
-			var string = "1+1";
+		it("should return 2 when given 1,1", function() {
+			var string = "1,1";
+			expect(calc.add(string)).to.equal(2);
+		})
 
-			expect(calculator.add(string)).to.equal(2);
-		});
-	});
-});
+		it("should return 3 when given 1,2", function() {
+			var string = "1,2";
+			expect(calc.add(string)).to.equal(3);
+		})
+
+		it("should return 4 when given 2,2", function() {
+			var string = "2,2";
+			expect(calc.add(string)).to.equal(4);
+		})
+
+	})
+})
